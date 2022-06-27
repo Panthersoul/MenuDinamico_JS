209b97825e49d0f3516a58f20e8d36cceda69b37
Andres Giacosa <giacosa.andres@gmail.com>
2022-06-27 15:36:32 -0300

    seteo el ambiente con un html basico, js basico y mas cosas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("hola")
+let numero1
+let numero2 
+
+do{
+    numero1 = prompt("ingrese un nro")
+    numero2 = prompt("ingrese otro nro")
+}while ((numero1.toUpperCase() == "ESC") || (numero2.toUpperCase() == "ESC") || numero1.isNaN || numero2.isNaN)
+
+
+
+
+if (numero1 >= numero2){
+ alert("El primer numero no puede ser mayor o igual que el segundo")
+}else if (numero1 < numero2){
+    for (let i = numero1; i <= numero2; i++){
+        console.log(i)
+    }
+}
