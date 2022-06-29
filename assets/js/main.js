84a3cec486103ac10025806764882be38a9a2325
Andres Giacosa <giacosa.andres@gmail.com>
2022-06-29 16:56:59 -0300

    adivinanza

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,47 @@
-let numero1
-let numero2 
+let nombre = prompt("Ingresa tu nombre: ")
+alert("Iremos por una adivinanza: <br>")
 
 do{
-    numero1 = prompt("ingrese un nro")
-    numero2 = prompt("ingrese otro nro")
-}while ((numero1.toUpperCase() == "ESC") || (numero2.toUpperCase() == "ESC") || numero1.isNaN || numero2.isNaN)
+    nro1 = prompt("Ingresa el número inicial (o ESC para salir): ")    
+    if (isNaN(nro1)){ 
+        alert("No por favor, ingresa un número correcto.")
+        location.reload();
+    }
+    nro2 = prompt("Ingresa el número final (o ESC para salir): ")    
+    if (isNaN(nro2) && (nro2 > nro1)){ 
+        alert("El segundo ingreso debe ser numerico y mayor que el primer nro")
+        location.reload();
+    }
+    if (nro1 >= nro2){
+        alert("El primer numero no puede ser mayor o igual que el segundo")
+       }else if (nro1 < nro2){
+           for (let i = nro1; i <= nro2; i++){
+               console.log(i)
+           }
+       }
+       
+}while ((nro1.toUpperCase() == "ESC") || (nro2.toUpperCase() == "ESC"))
+
+/*
+-Tiene dientes y no come, tiene cabeza y no es hombre. (Ajo)
 
 
 
 
-if (numero1 >= numero2){
+alert("Hola... aquí podes sacar tu entrada.")
+let nombre = prompt("Escribe tu nombre para empezar: ")
+let pelicula = prompt("Ahora, escribe la pelicula que quieres ver")
+let edad = prompt("Edad?")
+let compas = prompt("Por ultimo, la cantidad de compañeros")
+
+
+
+*/
+
+if (nro1 >= nro2){
  alert("El primer numero no puede ser mayor o igual que el segundo")
-}else if (numero1 < numero2){
-    for (let i = numero1; i <= numero2; i++){
+}else if (nro1 < nro2){
+    for (let i = nro1; i <= nro2; i++){
         console.log(i)
     }
 }
