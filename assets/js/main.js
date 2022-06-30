8048d09dcad5615f9b3dd2b7759d0ecf78497bea
Andres Giacosa <giacosa.andres@gmail.com>
2022-06-29 22:47:38 -0300

    corrijo link

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ const respuesta = "AJO"
 let contador = 0
 let acierto = false
 let intento = ""
-
+let respuestas = []
 /*Esta variable la uso para salir del loop*/
 let salgo = true
 
@@ -13,6 +13,8 @@ alert("OK " + nombre + ", iremos por una adivinanza. Tendrás solo 3 intentos.")
 
 do{
     intento = prompt("Si me arrancas la piel no lloraré, ¡pero tú sí!, ¿Quién soy?")    
+    
+    respuestas.push(intento)
 
     if (intento.toUpperCase() === respuesta){ 
         acierto = true
@@ -46,5 +48,7 @@ do{
 if (acierto)
         alert("Lo has hecho muy bien!")
    else if (acierto == false)
-        alert("Intenta de nuevo recargando la pagina! Buena suerte la proxima!")
-    
+    for (let i = 0; i < respuestas.length; i++){
+        console.log("Respuesta "+[i + 1]+" : "+respuestas[i])    
+    }
+    alert("Intenta de nuevo recargando la pagina! Buena suerte la proxima!")
