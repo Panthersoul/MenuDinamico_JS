8675bfaa22560b0e0d867cb56cd6817164fe7410
Andres Giacosa <giacosa.andres@gmail.com>
2022-07-07 15:01:16 -0300

    agrego condicionales de null

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-/* La idea es crear un menú para restaurant dinámico, agregando categorias y a esas categorias platos con su precio. */
+/*  La idea es crear un menú para restaurant dinámico, 
+    agregando categorias y a esas categorias 
+    platos con su respectivo precio. */
 
 //El producto esta asociado a una categoria
 class producto {
@@ -6,6 +8,10 @@ class producto {
         this.categoria = categoria;
         this.nombre = nombre;
         this.precio = precio;
+    }
+
+    iva(){
+        return this.precio * 1.22;
     }
 
 }
@@ -28,30 +34,35 @@ let nroCat = 1;
 
 do {
     let nombreCat = prompt("Ingresa una categoría: ");    
-    if (nombreCat != nombreCat.toUpperCase("ESC")) 
+    if (nombreCat != "null") 
     {
         unaCategoria = new categoria(nombreCat, nroCat);
+        console.log(unaCategoria);
         listaCategoria.push(unaCategoria);
         nroCat++;
     }
 }while(confirm(`¿Desea seguir agregando categorías?`))
 
-alert(`Ok... ahora, agregaremos platos con su precio a cada categoría.`);
+alert(`Ok... ahora, agregaremos PLATOS y su PRECIO a cada categoría.`);
 
 let salgo = 0;
+
 do {
     for (cat of listaCategoria){
         alert(`Ahora agregaremos productos a la categoría: ${cat.nombre}`)
         do {
             let nombreProd = prompt(`Nombre del plato para Menú: ${cat.nombre}`);
-            let precio = prompt("Precio");
-            let unProducto = new producto(cat.numero, nombreProd, precio);
-            listaProductos.push(unProducto);
+            if(nombreProd != null){
+                let precio = prompt("Precio");
+                let unProducto = new producto(cat.numero, nombreProd, precio);
+                listaProductos.push(unProducto);
+            }
         }while(confirm(`¿Desea agregar otro producto a la categoría ${cat.nombre}?`))
         salgo++;
     }
 }while( salgo != listaCategoria.length)
 
+console.log(listaProductos[0].iva());
 console.log(listaCategoria);
 console.log(listaProductos);
 
