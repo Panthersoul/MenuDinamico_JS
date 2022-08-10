a50e0d79c3f43d29d9ba10a209a427d5470e56f4
Andres Giacosa <giacosa.andres@gmail.com>
2022-08-10 13:19:40 -0300

    agrego enviar comanda

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -3,21 +3,17 @@ let arc = [];
 
 let articulos = document.querySelectorAll(".carrito");
 
-const carrito = [];
+
 const agregoCarrito = ( {target} ) => {
-    
+    let carrito = [];
     let NombreProdCarrito = target.querySelector(".nomElemCarr");
     let PrecioProdCarrito = target.querySelector(".precioElemCarr");
-    
     let nuevoProdNom = document.createElement("p");
     nuevoProdNom.className = "carritoNombre";
     nuevoProdNom.innerHTML = NombreProdCarrito.innerHTML;
-    
     let nuevoProdPrecio = document.createElement("p");
     nuevoProdPrecio.className = "carritoPrecio";
     nuevoProdPrecio.innerHTML = PrecioProdCarrito.innerHTML;
-
-    
     let productoCarro = {
         nombre: nuevoProdNom.innerHTML,
         precio: nuevoProdPrecio.innerHTML.substring(1,nuevoProdPrecio.innerHTML.length).trim()
@@ -35,6 +31,8 @@ const agregoCarrito = ( {target} ) => {
         let carrin = document.querySelector(".carrito");
         carrin.classList.remove("d-none");
     }
+
+    
 }
 
 /* Agrego al boton "VER CARRITO" los eventos que muestran cargan y eleminan*/
@@ -50,16 +48,27 @@ const quitarCarrito = ( {target} ) => {
     cargarCarrito();
 }
 
-cargarCarrito = () => {
+const limpiarCarritoHTML = () => {
     let modalCuerpo = document.getElementById("modal-cuerpo");
     let modalTotal = document.getElementById("modal-total");
-    modalCuerpo.removeChild(modalCuerpo.firstChild);
-    modalTotal.removeChild(modalTotal.firstChild);
-    const listado = JSON.parse(localStorage.getItem("carrito"));
+    if (modalCuerpo.hasChildNodes()){
+        modalCuerpo.removeChild(modalCuerpo.firstChild);
+    }
+    if (modalTotal.hasChildNodes())
+    {
+        modalTotal.removeChild(modalTotal.firstChild);
+    }
+    
+}
+
+cargarCarrito = () => {
+    limpiarCarritoHTML();
+
+    let listado = JSON.parse(localStorage.getItem("carrito"));
     let ul = document.createElement("ul");
     let total = 0;
     let monto = [];
-
+    
     listado.forEach( elem => { 
         let li = document.createElement("li");
         total = total + parseInt(elem.precio);
@@ -74,7 +83,6 @@ cargarCarrito = () => {
     suma.className = "totalCarro";
     suma.innerHTML = `El total del pedido es $<span class="verde margenIZ">${sumatoria}</span>`;
     suma.classList.add("p-3");
-
     modalTotal.appendChild(suma);
     modalCuerpo.appendChild(ul);
 
@@ -82,6 +90,7 @@ cargarCarrito = () => {
     for (art of artiCarrito){
         art.addEventListener("click", quitarCarrito)
     }
+    listado = null;
 
 }
 
@@ -92,4 +101,42 @@ function calcularTotal (...monto){
 verCarro.addEventListener("click", ()=>{
     cargarCarrito();   
 })
+
+let btnPedido = document.getElementById("enviarComanda");
+let cerrarModal = document.getElementById("cerrarModal");
+let btnCarrito = document.getElementById("carrito");
+let modalCuerpo = document.getElementById("modal-cuerpo");
+let modalTotal = document.getElementById("modal-total");
+
+
+const enviarComanda = () => {
+    const articulos = JSON.parse(localStorage.getItem("carrito"));
+    
+    fetch('https://jsonplaceholder.typicode.com/posts',
+    {
+        method: 'POST',
+        body: JSON.stringify({
+            articulos
+        }),
+        headers: {
+            'Content-type':'application/json;charset=UTF-8',
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        btnCarrito.classList.add("d-none");
+        Swal.fire( {title: "Hemos enviado la comanda... <br> la puedes ver en consola.", imageUrl: '../images/pizza.jpg', imageAlt: 'ImagenAlerta'})
+        localStorage.removeItem("carrito");
+        limpiarCarritoHTML();
+        cerrarModal.click();
+    })
+    .catch((error) => {
+        Swal.fire( {title: error, imageUrl: '../images/error.jpg', imageAlt: 'ImagenAlerta'})
+    })
+}
+
+
+btnPedido.onclick = enviarComanda;
+
 
